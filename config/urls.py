6dd161806c6cef339b9b098e4da684f71632762d
SDE <deanisik@gmail.com>
2025-07-23 11:44:15 +0700

    add urls to config

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("task/", include("task_app.urls")),
+    path("", include("user_app.urls")),
 ]
