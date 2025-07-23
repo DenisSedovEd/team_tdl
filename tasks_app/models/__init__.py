b67c7c5f16cb10349c76d4c27cc8c96bbdccac5a
SDE <deanisik@gmail.com>
2025-07-23 09:08:34 +0700

    delete tasks_app

--- tasks_app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from tasks_app.models.user import User as User
-from tasks_app.models.task_list import TaskList as TaskList
-from tasks_app.models.task import Task as Task
-from tasks_app.models.task_status import TaskStatus as TaskStatus
--- tasks_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from .views.user import login_view, register_view
-
-urlpatterns = [
-    path("register/", register_view, name="register"),
-    path("login/", login_view, name="login"),
-]
